6db1f24919a4ab2bb98868f1d0dde0f0112ec4b7
Shiva Dwivedi <shivbhushan703@gmail.com>
2024-07-23 17:20:50 +0530

    fixed node issue

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/apps/proxy', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/register-sw.html'), err => {
+    res.sendFile(path.join(__dirname, 'public/service-worker.js'), err => {
         if (err) {
             console.log("=====>please deploy the existing one")
             console.error(err);

15ec5d7f4db7cc9d915750839bded78b39c8e5b5
Shiva Dwivedi <shivbhushan703@gmail.com>
2024-07-23 16:42:14 +0530

    added file to app proxy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/service-worker.js', (req, res) => {
+app.get('/app/proxy', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/service-worker.js'), err => {
         if (err) {
             console.log("=====>please deploy the existing one")
